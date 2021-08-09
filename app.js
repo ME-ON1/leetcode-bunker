9f8aa681a85fe5a94c4abbd954487606d043dec0
Tarun Sharma <starun.1998@gmail.com>
2021-08-09 15:05:38 +0530

    added the filehdl and fmtHdl

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
+require("dotenv").config()
 const axios = require("axios").default
 const fs = require("fs")
-require("dotenv").config()
 
 const path = require("path")
 const util = require("util")
+
+const respJSON = require("../leetcoderes.json")
+
 const URL = "https://leetcode.com/api/submissions"
 const PROBLEM_URL = "https://leetcode.com/api/problems/all"
 
 
-const cookieVal = process.env.COOKIE_SECRET || "nothing" ;
+const cookieVal = "LEETCODE_SESSION=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfYXV0aF91c2VyX2lkIjoiMjA3Mzc3NSIsIl9hdXRoX3VzZXJfYmFja2VuZCI6ImFsbGF1dGguYWNjb3VudC5hdXRoX2JhY2tlbmRzLkF1dGhlbnRpY2F0aW9uQmFja2VuZCIsIl9hdXRoX3VzZXJfaGFzaCI6IjU3MGFiN2MwNDI5MDk4YjUyZjA2OTFlMWJmMzliODBjNjM0NjE2YWIiLCJpZCI6MjA3Mzc3NSwiZW1haWwiOiJzdGFydW4uMTk5OEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6Ik1lX09OIiwidXNlcl9zbHVnIjoiTWVfT04iLCJhdmF0YXIiOiJodHRwczovL3d3dy5ncmF2YXRhci5jb20vYXZhdGFyL2U5MTdjYWMzMWY5NzZhMDYyM2IxNDYwMTk2ZjRmMDFiLnBuZz9zPTIwMCIsInJlZnJlc2hlZF9hdCI6MTYyODQ4MjU0OCwiaXAiOiI2MS4wLjM1Ljk4IiwiaWRlbnRpdHkiOiIyOTI4YWM4YWMwMjNiOTdmMzljMDMzZDM1YjllZDE3NiIsInNlc3Npb25faWQiOjg0NTE4MjAsIl9zZXNzaW9uX2V4cGlyeSI6MTIwOTYwMH0.qgfaGy7PebjYmheNbVOJOJIeRc0OGF-0oGdmNLQt2G8; Domain=.leetcode.com; expires=Mon, 23 Aug 2021 04:15:48 GMT; HttpOnly; Max-Age=1209600; Path=/; SameSite=Lax; Secure"
 
 const readFileDir = util.promisify(fs.readdir)
 const codeWrite = util.promisify(fs.writeFile)
 let aldyPresentSol = {}
 
-const mapFilePresentInDir = async (  )=>{
+;(async (  )=>{
 	try {
 		const subPresent = await readFileDir(__dirname) ;
 		subPresent.map(val => {
@@ -29,8 +32,54 @@ const mapFilePresentInDir = async (  )=>{
 	{
 		return new Error("some err ", Er)
 	}
+})()
+
+function SolutionDetails ({id, lang , runtime, memory, code, title_slug})
+{
+	this.id = id
+	this.lang = lang
+	this.memory = memory
+	this.runtime = runtime
+	this.code = code
+	this.title_slug = title_slug
+	this.fmtData = ""
 }
 
+SolutionDetails.prototype.IsPresent = ()=>{
+	if(aldyPresentSol[this.id] === 1 )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+SolutionDetails.prototype.fmtHdl = function(){
+	this.fmtData += `id = ${this.id}` ;
+	this.fmtData += '\n'
+	this.fmtData += `runtime  = ${this.runtime} \n`
+	this.fmtData += `memory = ${this.memory}\n`
+	this.fmtData += `title_slug = ${this.title_slug}\n`
+	this.fmtData += `code =\n \`\`\`${this.code}\`\`\``
+	this._fileWriteHdl()
+}
+
+SolutionDetails.prototype._fileWriteHdl = async function() {
+	let vs_sol = this.id ;
+	//if(!fs.existsSync(`./${this.id}_${this.title_slug}`))
+	//{
+		try {
+			await codeWrite(`${this.id}_${this.title_slug}.md`,this.fmtData)
+			aldyPresentSol[this.id] = 1 ;
+		}
+		catch(er)
+		{
+			throw er;
+		}
+	//}
+}
 // two func
 // 1 should fetch every information ProblemURL
 // 	check if file exist if not then
@@ -71,63 +120,28 @@ const mapFilePresentInDir = async (  )=>{
 //})();
 
 ;(async function (){
-        mapFilePresentInDir() ;
-	const r_recentSubmittedSols = await axios({
-		method : 'GET',
-		baseURL : URL,
-		headers : {
-			cookie : cookieVal
-		}
-	})
-	r_recentSubmittedSols.data.submissions_dump.map(bVal => {
-		let r_submittedSol = {}
+	//const r_recentSubmittedSols = await axios({
+		//method : 'GET',
+		//baseURL : URL,
+		//headers : {
+			//cookie : cookieVal
+		//}
+	//})
+	respJSON/*r_recentSubmittedSols.data*/.submissions_dump.map(bVal => {
 		if(bVal.status_display === 'Accepted')
 		{
-			const {
-				id ,
-				lang,
-				runtime,
-				memory,
-				title_slug,
-				code
-			} = bVal ;
+			const sol_obj = new SolutionDetails(bVal);
 
-			r_submittedSolObj = Object.assign(r_submittedSol , {id, lang , runtime, memory , title_slug, code})
-			filePresenceHdl(r_submittedSol)
+			if(!sol_obj.IsPresent())
+			{
+				sol_obj.fmtHdl()
+
+			}
 		}
 	})
 
 
 })()
-
-const filePresenceHdl = (r_sol ) =>{
-	if(aldyPresentSol[r_sol.id] === 1)
-	{
-		return ;
-	}
-	else
-	{
-		fileWriteHdl(r_sol)
-	}
-}
-
-const fileWriteHdl = async (r_sol) =>{
-	let s = ""
-	let p = r_sol.code.split("\n")
-
-	p.forEach(val =>{
-		let nw = "\t \t" + val ;
-		return val  = nw ;
-	})
-
-	console.log(p , "whas")
-
-	p.forEach(val => console.log(val))
-	//s = `#### Solution Id is - ${r_sol.id} \n #### ProblemTitle - ${r_sol.title_slug} \n #### RunTime - ${r_sol.runtime}\n #### memory - ${r_sol.memory} \n #### langauge - ${r_sol.lang} \n \n #### code - \n \t \t \t  ${r_sol.code}`
-	//console.log(s)
-	////await codeWrite(`${r_sol.id}_${r_sol.title_slug}.md`, s) ;
-}
-
 
 process.on('exit', ()=>{
 	console.log("sdf")
